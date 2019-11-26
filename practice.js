f3e64c1bdd13eca9aa7409db948539017015658b
wggreen <wggreen.nss@gmail.com>
2019-11-26 11:50:15 -0600

    first commit

---
+++ practice.js
@@ -0,0 +1,11 @@
+const nameMyFavoriteDogBreed = (breed) => {
+    if (breed === 'schnauzer') {
+        const result = "My favorite dog breed is schnauzer"
+    }
+    else if (breed === 'meow') {
+        const result = "I like cats"
+    }
+    return result
+}
+
+const favoriteBreed = nameMyFavoriteDogBreed()
